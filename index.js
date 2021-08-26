1ab28bdc3315bf5d198ddab86cc728599c9234b1
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-08-27 00:50:37 +0100

    added old code

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js")
+
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] })
+
+const loadCommands = require("./commands/load-commands");
+const botConfig = require("./configs/botConfig.json");
+const music = require("./musicFunctions")
+
+let servers = {};
+
+client.on("ready", async () => {
+    console.log(`Zayden is Running, version: ${botConfig.version}`);
+
+    client.user.setPresence({
+        activity: {
+            name: "College Kings",
+            type: 0,
+        },
+    })
+
+    const guilds = client.guilds.cache.map(guild => guild.id)
+    for (guild of guilds) {
+        servers[guild].queue = new music.Queue(guild)
+    }
+    module.exports = { servers: servers }
+
+    loadCommands(client)
+
+});
+
+client.on("disconnect", () => {
+    console.log("Bot shutting down.")
+})
+
+client.on("error", error => {
+    console.log(`Error Encountered`);
+})
+
+client.login(botConfig.token)
